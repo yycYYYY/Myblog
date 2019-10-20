0e442b9d81a8959a63cfe80a69c4c5a0de287e9e
matthewot <616539757@qq.com>
2019-10-20 23:40:21 +0800

    完成一点

--- src/main/java/com/oneblog/blog/controller/ArticleController.java
+++ src/main/java/com/oneblog/blog/controller/ArticleController.java
@@ -1,11 +1,15 @@
 package com.oneblog.blog.controller;
 
 import com.oneblog.blog.tools.Msg;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ArticleController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ArticleController.class);
 
     @GetMapping(value = "/articles")
     public Msg get(){
--- src/main/java/com/oneblog/blog/controller/LoginController.java
+++ src/main/java/com/oneblog/blog/controller/LoginController.java
@@ -1,4 +1,11 @@
 package com.oneblog.blog.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class LoginController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 }
